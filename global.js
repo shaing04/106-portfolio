d57c1d50d676deee55a51a3d272b621121960066
Susana <shaing@ucsd.edu>
2025-10-16 09:31:19 -0700

    checking js for nav bar highlight

--- global.js
+++ global.js
@@ -1,16 +1,14 @@
 // global.js
-console.log('IT’S ALIVE!');
+console.log('js test 1');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a").filter(a => a.target !== "_blank");
+const navLinks = $$("nav a").filter(a => a.target == "_blank");
 
-let currentLink = navLinks.find(a => {
-  let linkPath = a.pathname.replace(/index\.html$/, "");
-  let currentPath = location.pathname.replace(/index\.html$/, "");
-  return a.host === location.host && linkPath === currentPath;
-});
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
 
 currentLink?.classList.add("current");
