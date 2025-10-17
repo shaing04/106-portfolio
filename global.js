c84f0543c37a96bf2a64b3cf8acc1f40f56bb4d8
Susana <shaing@ucsd.edu>
2025-10-16 21:25:39 -0700

    updated js to automate nav bar updates

--- global.js
+++ global.js
@@ -1,23 +1,34 @@
 // global.js
-console.log('js test 5');
+console.log('js test new nav bar');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a").filter(a => a.target !== "_blank");
+// const navLinks = $$("nav a").filter(a => a.target !== "_blank");
 
-let currentLink = navLinks.find(a => {
-  let linkPath = a.pathname.replace(/index\.html$/, "");
-  let currentPath = location.pathname.replace(/index\.html$/, "");
-  return a.host === location.host && linkPath.endsWith(currentPath);
-});
+// let currentLink = navLinks.find(a => {
+//   let linkPath = a.pathname.replace(/index\.html$/, "");
+//   let currentPath = location.pathname.replace(/index\.html$/, "");
+//   return a.host === location.host && linkPath.endsWith(currentPath);
+// });
 
-currentLink?.classList.add("current");
+// currentLink?.classList.add("current");
 
-currentLink?.classList.add("current");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume'},
+  { url: 'contact/', title: 'Contact'}, 
+  // add the rest of your pages here
+];
 
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-console.log(location.pathname);
-console.log(navLinks.map(a => a.pathname));
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
 
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
