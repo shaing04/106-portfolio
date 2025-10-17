5122a3d6d2147cd0be3a5bb357168a7cd539cd3d
Susana <shaing@ucsd.edu>
2025-10-16 21:30:11 -0700

    file root check

--- global.js
+++ global.js
@@ -32,3 +32,7 @@ for (let p of pages) {
 
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
