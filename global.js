201a1d5622e0c1a261dd2596ce714dcb8f87d2fc
Susana <shaing@ucsd.edu>
2025-10-17 14:33:09 -0700

    fix css for contact

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 // global.js
-console.log('js test contact form 2')
+console.log('js test contact form 3')
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
