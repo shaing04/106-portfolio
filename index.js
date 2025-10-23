d701daf43cb00506a8f7e08b9963ddfd073e90b3
Susana <shaing@ucsd.edu>
2025-10-22 23:12:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+//import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+import { fetchJSON, renderProjects } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
